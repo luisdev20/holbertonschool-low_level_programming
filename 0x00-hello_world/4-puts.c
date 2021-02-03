49e988a8be42c393c2c81d741aa67ba3bb1eee8e
luisdev20 <lescobedo.im@gmail.com>
2021-02-03 21:23:35 +0000

    Getting started with C programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - To put my main function
+ * @Mystr: The string to be printed
+ */
+int main(void)
+{
+    char Mystr[] = "Programming is like building a multilingual puzzle"
+
+    puts(Mystr);
+
+    return (0);
+}
