b62d2215e51f27d9b35886b3cdfd58c274dfc330
luisdev20 <lescobedo.im@gmail.com>
2021-02-03 22:21:25 +0000

    Getting started with C programming

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - To put my main function
- * return: a string
+ * Return: always 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar\, but the outcome is a piece of art\,\n");
 
 	return (0);
 }
