0b873febf1311c0ac6a9894b2ddeff57d9b55a11
luisdev20 <lescobedo.im@gmail.com>
2021-02-04 21:15:54 +0000

    Get expert in Control Structures ic C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,24 +8,24 @@
 int main(void)
 {
 	int n;
-	int last_digit;
+	int l_d;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	last_digit = n % 10;
+	l_d = n % 10;
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_d);
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, l_d);
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l_d);
 	}
 	return (0);
 }
