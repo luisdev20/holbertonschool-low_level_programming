9393d09e4cee06a8287fd4983667478efb908731
luisdev20 <lescobedo.im@gmail.com>
2021-02-05 01:33:15 +0000

    Get expert in Control Structures ic C

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - This programm prints digits from 0 to 9
+ * Return: returning 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
