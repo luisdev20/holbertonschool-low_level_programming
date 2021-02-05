df217b4cd4adb05086ad0bef53ffbe3c89b70f58
luisdev20 <lescobedo.im@gmail.com>
2021-02-05 00:17:16 +0000

    Get expert in Control Structures ic C

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This programm prints the alphabet in lowercase.
+ * Return: returning 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'A'; i <= 'Z'; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
