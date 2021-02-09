c8057a8851980ecb64acfb0469b51caae22546f0
luisdev20 <lescobedo.im@gmail.com>
2021-02-09 20:10:55 +0000

    getting started with debugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	/* while (i < 10)
 	{
 		putchar(i);
-	} /* This is the part that causes problems */
+	} */
 
 	printf("Infinite loop avoided! \\o/\n");
 
