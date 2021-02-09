104bea0ca6b079bfcbc7537c4549b637c088fee9
luisdev20 <lescobedo.im@gmail.com>
2021-02-09 11:26:41 +0000

    Learn how to use functions propertly

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * add - This programm adds two integers and returns the result.
+ * @a: First value to add.
+ * @b: Second value to add.
+ *
+ * Return: returning 0
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
