19e66179280abc036732b305927d771ee98aa6bc
luisdev20 <lescobedo.im@gmail.com>
2021-02-09 07:27:50 +0000

    Learn how to use functions propertly

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 #include "holberton.h"
 /**
- * main - This programm prints the alphabet in lowercase.
+ * print_alphabet - This programm prints the alphabet in lowercase.
  * Return: returning 0
  */
 void print_alphabet(void)
 {
 	char i;
-	int _putchar(char c);
 
 	for (i = 'a'; i <= 'z'; i++)
 	{
