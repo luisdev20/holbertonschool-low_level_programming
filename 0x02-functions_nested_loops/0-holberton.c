af0e70cab93b9c519a7a669979f004c2d959453f
luisdev20 <lescobedo.im@gmail.com>
2021-02-09 05:26:03 +0000

    Learn how to use functions propertly

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * main - This programm prints the alphabet in lowercase.
+ * Return: returning 0
+ */
+int main(void)
+{
+	int i;
+	char word[10] = {'H', 'o', 'l', 'b', 'e', 'r', 't', 'o', 'n', '\n'};
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	return (0);
+}
