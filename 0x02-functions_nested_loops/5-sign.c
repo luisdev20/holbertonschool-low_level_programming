bde1dfce4c4951864a6ab5a1c15f0dfdc20cb7b6
luisdev20 <lescobedo.im@gmail.com>
2021-02-09 09:05:45 +0000

    Learn how to use functions propertly

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * print_sign - This programm prints the sign of a number.
+ * @n: The entry to analize
+ *
+ * Return: returning 0
+ */
+int print_sign(int n);
+{
+	if (n >= 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
