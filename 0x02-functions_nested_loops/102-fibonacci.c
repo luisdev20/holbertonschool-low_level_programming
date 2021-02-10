8f34f6ed5f2b7bd13b217d6c43febe088bc12109
luisdev20 <lescobedo.im@gmail.com>
2021-02-10 18:29:28 +0000

    Learn how to use functions propertly

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - This programm prints the first 50 Fibonacci numbers
+ * starting with 1 and 2
+ *
+ * Return: void
+ */
+int main(void)
+{
+	int i;
+	int t1 = 1;
+	int t2 = 2;
+	int next_term;
+
+	for (i = 1; i <= 50; i++)
+	{
+		printf("%d", t1);
+		next_term = t1 + t2;
+		t1 = t2;
+		t2 = next_term;
+
+		if (i != 50)
+			printf(", ");
+	}
+	printf("\n");
+	return 0;
+}
