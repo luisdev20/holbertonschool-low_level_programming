a93f7338eff2dbc26ac982db5af3aa3ff22ecdf1
luisdev20 <lescobedo.im@gmail.com>
2021-02-11 23:29:12 +0000

    Here we go with more functions in C

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * main - This programm that prints the numbers from 1 to 100
+ * But for multiples of three print Fizz instead of the number and
+ * For the multiples of five print Buzz.
+ * For numbers which are multiples of both three and five print FizzBuzz.
+ * @size: Inserts the size of the square
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
