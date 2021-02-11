6e58bb254be8044d7ba63a764ce67278d2cbdeb7
luisdev20 <lescobedo.im@gmail.com>
2021-02-11 21:49:11 +0000

    Here we go with more functions in C

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * print_square - This programm that prints 10 times the numbers, from 0 to 14
+ * @size: Inserts the size of the square
+ *
+ * Return: no returning
+ */
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
