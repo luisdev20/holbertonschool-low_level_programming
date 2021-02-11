cef18cc2e884a3f32ec90a617d2fe2d0ef562eff
luisdev20 <lescobedo.im@gmail.com>
2021-02-11 19:35:44 +0000

    Here we go with more functions in C

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * print_line - This programm draws a straight line in the terminal.
+ * @n: The lenght of the line
+ *
+ * Return: no returning
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+			_putchar("_");
+		_putchar("\n");
+	}
+	else
+		_putchar("\n");
+}
