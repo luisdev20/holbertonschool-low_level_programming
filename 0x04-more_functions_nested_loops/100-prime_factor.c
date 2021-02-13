6083a8e3411c3e29e21a9cdcad7469b65ed41187
luisdev20 <lescobedo.im@gmail.com>
2021-02-13 00:07:04 +0000

    Here we go with more functions in C

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - This programm that pfinds and prints the largest
+ * prime factor of the number 612852475143.
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	long num = 612852475143;
+	long divisor = 2;
+
+	while (divisor < num )
+	{
+		if (num % divisor == 0)
+			num = num / divisor;
+		else
+			divisor++;
+	}
+	printf("%ld\n", num);
+	return (0);
+}
