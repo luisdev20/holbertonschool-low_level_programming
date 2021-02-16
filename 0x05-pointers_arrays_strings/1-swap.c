ec6ae499a367021e94ef08c74c86032bb69baaf9
luisdev20 <lescobedo.im@gmail.com>
2021-02-16 21:02:01 +0000

    Learning Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * void swap_int - This programm swaps the values of two integers.
+ * @a: The number one to swap.
+ * @b: The number two to swap.
+ *
+ * Return: no returning.
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	c = *a;
+
+	*a = *b;
+	*b = *(&c);
+}
