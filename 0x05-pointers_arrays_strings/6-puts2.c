6147900300594e7dd3fcf47b062a233174a68664
luisdev20 <lescobedo.im@gmail.com>
2021-02-17 04:42:21 +0000

    Learning Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * puts2 - This programm that prints every other character of a string.
+ * starting with the first character, followed by a new line.
+ * @str: The string to analize.
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i = i + 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
