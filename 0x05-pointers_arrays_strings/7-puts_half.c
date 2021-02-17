4a9793815a9ff80fd074bdf38d4142fd4830b1ae
luisdev20 <lescobedo.im@gmail.com>
2021-02-17 05:53:22 +0000

    Learning Pointers, Arrays and Strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,21 +9,23 @@ void puts_half(char *str)
 {
 	int i;
 	int c = 0;
-	int len;
+	int len, par, impar;
 
 	while (str[c] != '\0')
 		c++;
 
-	len = c;
+	len = c - 1;
+	par = len / 2;
+	impar = (len - 1) / 2;
 
 	if (len % 2 == 0)
 	{
-		for (i = len / 2; i <= len; i++)
+		for (i = len - par; i <= len; i++)
 			_putchar(str[i]);
 	}
 	else
 	{
-		for (i = (len - 1) / 2; i <= len; i++)
+		for (i = len - impar; i <= len; i++)
 			_putchar(str[i]);
 	}
 	_putchar('\n');
