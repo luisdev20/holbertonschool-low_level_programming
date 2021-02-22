27e4c082288cf6b61738a63a40cf3f377c2d8780
luisdev20 <lescobedo.im@gmail.com>
2021-02-22 22:02:46 +0000

    Getting advanced with pointers and arrays

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,14 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] != '\0'; i++)
+	i = 0;
+	while (s[i])
 	{
 		if (s[i] == c)
 		{
 			return (&s[i]);
 		}
+		i++;
 	}
 	return (NULL);
 }
