64e0af4aac76924fd9af0dfedc2c2718428c85f2
luisdev20 <lescobedo.im@gmail.com>
2021-02-22 21:22:27 +0000

    Getting advanced with pointers and arrays

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "holberton.h"
+/**
+ * _strchr - Function that fills memory with a constant byte.
+ * @s: pointer to a char.
+ * @c: constant byte.
+ *
+ * Return: returning a pointer to a char.
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			s = &s[i];
+			return (s);
+		}
+	}
+	return (0);
+}
