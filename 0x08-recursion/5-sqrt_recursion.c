6f3903b615670bf28cb950dcd7a7072eaf91ba96
luisdev20 <lescobedo.im@gmail.com>
2021-02-24 22:40:34 +0000

    Learning Recursion hardar

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * _sqrt_recursion - F that returns the natural square root of a number.
+ * _sqrt - Function to calculate sqrt recusive.
+ * @n: base number
+ * @i: base number that iterate.
+ *
+ * Return: an int.
+ */
+int _sqrt(int i, int n)
+{
+	if (i < 0)
+		return (-1);
+
+	if (i * i == n)
+		return (i);
+
+	return (_sqrt(i - 1, n));
+}
+
+int _sqrt_recursion(int n)
+{
+	int h;
+	int i = n;
+
+	h = _sqrt(i, n);
+	return (h);
+}
