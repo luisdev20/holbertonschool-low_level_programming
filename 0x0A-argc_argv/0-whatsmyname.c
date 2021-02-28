bebaa620821bd770abb67955d1078340c29997da
luisdev20 <lescobedo.im@gmail.com>
2021-02-28 18:16:03 +0000

    Writting functions in command line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+int main(int __attribute__((unused)) argc, char **argv)
+{
+	printf("%s\n", *argv);
+	return (0);
+}
