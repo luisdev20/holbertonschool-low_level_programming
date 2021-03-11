32db99477d75378e17d6c0874109ccd364879e06
luisdev20 <lescobedo.im@gmail.com>
2021-03-11 04:25:32 +0000

    Learning function pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * array_iterator - Function that executes a function given as a parameter
+ * on each element of an array.
+ * @array: array
+ * @size: size of the array
+ * @action: pointer to the function you need to use.
+ *
+ * Return: always void.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size > 0)
+	{
+		action(*array);
+		array ++;
+		size --;
+	}
+}
