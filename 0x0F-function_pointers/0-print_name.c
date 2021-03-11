6c0789415ef654d0ffc140e9fe19be4679439f60
luisdev20 <lescobedo.im@gmail.com>
2021-03-11 03:16:28 +0000

    Learning function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+ * print_name - A new struct describing a dog.
+ * @name: string to print.
+ * @f: pointer of function f.
+ *
+ * Return: always void.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
