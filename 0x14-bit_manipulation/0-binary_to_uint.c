7e953b83e98646606196509a28ecac0378547038
Luis David Escobedo Velasquez <lescobedo.im@gmail.com>
2021-03-25 19:36:27 -0500

    Update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	for (len = 0; b[len]; len++)
 	{}
 
-	for (i = len - 1; len >= 0; i--)
+	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
