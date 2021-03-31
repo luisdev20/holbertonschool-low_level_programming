c1b243888defbaa92fc48bb58b31daff3e5ee02c
luisdev20 <lescobedo.im@gmail.com>
2021-03-30 21:34:20 -0500

    Creating a new fle

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
 
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 	if (numwritten == -1)
 		return (-1);
 
-	close (fd);
+	close(fd);
 
 	return (1);
 }
