4c449cc00628c190429228db72cbc7c5d79e771d
Luis David Escobedo Velasquez <lescobedo.im@gmail.com>
2021-04-08 13:33:09 -0500

    Create 0-print_arg.c

---
+++ testing_bash/0-print_arg.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(int __attribute__((unused)) ac, char **av)
+{
+        int i;
+
+        for (i = 0; av[i]; i++)
+        {
+                printf("%s ", av[i]);
+        }
+        printf("\n");
+}
