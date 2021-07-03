1fa68660f2ff77132fc2df3b88a9f1e391d4fa46
luisdev20 <lescobedo.im@gmail.com>
2021-07-03 02:47:41 +0000

    [ADD] makefile

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Represent an island perimeter measure function."""
+
+
+def island_perimeter(grid):
+    """Returns an island representation where:
+
+    Args:
+        Grid (list): List of integers.
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid is completely surrounded by water.
+
+    Returns: the perimeter of the island.
+    """
+
+    w = len(grid[0])
+    h = len (grid)
+    edges = 0
+    size = 0
+
+    for i in range(h):
+        for j in range(w):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,31 @@
+# 0x1C. C - Makefiles
+
+## Tasks
+
+### 0. make -f 0-Makefile
+Create your first Makefile.
+* File: 0-Makefile
+
+### 1. make -f 1-Makefile
+make -f 1-Makefile
+* 1-Makefile
+
+### 2. make -f 2-Makefile
+Create your first useful Makefile.
+* File: 2-Makefile
+
+### 3. make -f 3-Makefile
+make -f 3-Makefile
+* File: 3-Makefile
+
+### 4. A complete Makefile
+rules: all, clean, fclean, oclean, re
+* File: 4-Makefile
+
+### 5. Island Perimeter
+Create a function def island_perimeter(grid): that returns the perimeter of the island described in grid.
+* File: 5-island_perimeter.py
+
+### 6. make -f 100-Makefile
+ADvanced task
+* File: 100-Makefile
